2941447b86a1a4508d7d8ed1d9144f9a1e68590f
musicboy17 <pratham@yandex.ru>
2022-08-07 18:05:15 +0300

    Ex 003 Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// int numberA = 3;
+// int numberB = 5;
+// 
+// Console.WriteLine(numberA + numberB);
+
+int numberA = 31;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
