8a1eea0b9f2e6de115acdad9fe0e63ecf0570dae
musicboy17 <pratham@yandex.ru>
2022-08-07 19:06:01 +0300

    Ex 005 Name

---
+++ Example005_Name/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+   Console.WriteLine("УРА! Это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
